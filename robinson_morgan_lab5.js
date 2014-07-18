33adec611c149ae2c01c310962d8cf5e5c6aa774
MLRobinson3995 <MLRobinson@fullsail.edu>
2014-07-17 22:26:58 -0400

    7/17/14 Lab5

---
+++ robinson_morgan_lab5.js
@@ -0,0 +1,86 @@
+//  Morgan Robinson
+//  07/17/2014
+// Scalable Data Infrastructures 1407
+//  Day 5 Lab
+
+//varibles
+
+var num1,
+	num2,
+	i,
+	operation;
+
+//divide
+
+var div = function(x, y) {
+	
+	var end;
+	end = (x / y);
+	return end;
+	
+};
+
+//multiply
+
+var mul = function(x, y) {};
+
+//subract
+
+var sub = function(x, y) {};
+
+//add
+
+var add = function(x, y) {};
+
+//prompts
+
+num1 = parseInt(prompt("What is the first number?", "0"));
+num2 = parseInt(prompt("What is the secound number?", "0"));
+i = parseInt(prompt("What is the operation?\n1.Division\n2.Multiplication\n3.Subtraction\n4.Addition\nEnter the number shown. "));
+
+if (i = 1) {
+	
+	operation = div(num1, num2);
+	
+} else if (i = 2){
+	
+	operation = mul(num1, num2);
+	
+} else if (i = 3){
+	
+	operation = sub(num1, num2);
+	
+} else if (i = 4){
+	
+	operation = add(num1, num2);
+	
+}
+
+
+//output
+
+
+if (i = 1) {
+	
+	if ((num1 === 0) || (num2 === 0)){
+		
+		console.log (num1 + " divided by " + num2 + " equals 0");
+		
+	} else {
+		
+		console.log (num1 + " divided by " + num2 + " equals " + operation);
+		
+	}
+	
+} else if (i = 2){
+	
+	
+	
+} else if (i = 3){
+	
+	
+	
+} else if (i = 4){
+	
+	
+}
