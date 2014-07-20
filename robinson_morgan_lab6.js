5b3d819d21aadfeffcfeb3962222cb12d77a7617
MLRobinson3995 <MLRobinson@fullsail.edu>
2014-07-19 21:40:07 -0400

    Arrays done

---
+++ robinson_morgan_lab6.js
@@ -0,0 +1,20 @@
+///  Morgan Robinson
+//  07/17/2014
+// Scalable Data Infrastructures 1407
+//  Day 5 Lab
+
+var realName,
+	alias;
+	
+
+var hero = function() {
+	
+	var heroName = new Array ("Superman", "Batman", "Wonder Woman", "Flash", "Martian Manhunter", "Aquaman", "Hawkman");
+			
+};
+
+var civilan = function() {
+	
+	var secretIdnetity = new Array ("Clark Kent", "Bruce Wayne", "Diana Prince", "Barry Allen", "John Jones", "Arthor Curry", "Carter Hall");
+			
+};
