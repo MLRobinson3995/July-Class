377a099be8efc1abac547b1b9c66c6ce611697fc
MLRobinson3995 <MLRobinson@fullsail.edu>
2014-07-22 22:06:58 -0400

    7/22/14 Lab7 json added

---
+++ animal_json.js
@@ -0,0 +1,39 @@
+var jsonData = {
+	
+	"animals": 
+	[ 
+		{
+			"name": "Cheetah",
+			"sciName": "Acinonyx jubatus",
+			"topSpeed": 75
+		
+		
+			
+		},
+		{
+			"name": "Peregirn Falcon",
+			"sciName": "Falco peregrinus",
+			"topSpeed": 242
+			
+			
+		},
+		{
+			"name": "Black Marlin",
+			"sciName": "Istiompax indica",
+			"topSpeed": 80
+		
+		},
+		{
+			"name": "Horse fly",
+			"sciName": "family Tabanidae",
+			"topSpeed": 90"
+			
+		},
+		{
+			"name": "Mexican Free-Tailed Bat ",
+			"sciName": "Tadarida brasiliensis",
+			"topSpeed": 60
+			
+		}
+	]
+};
