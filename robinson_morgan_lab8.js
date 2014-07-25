0482aecc4735f2b33fd693133bb3477d7f8e1879
MLRobinson3995 <MLRobinson@fullsail.edu>
2014-07-24 23:50:23 -0400

    7/24/14 Lab8 p1, p2 done

--- robinson_morgan_lab8.js
+++ robinson_morgan_lab8.js
@@ -4,15 +4,120 @@
 //  Day 8 Lab
 
 //problem 1
-
-var text = "thisaddress@gmail.com";
-
-var addConfirm = function(address) {
+/*
+	//variables
+	var text = "thisaddress@gmail.com";
+	var end;
 	
+	//function
+	var addConfirm = function(address) {
+
+		//at1
+		var atThere = address.indexOf("@");
+		
+		if (atThere === -1) {
+			
+			var atIn = false;
+			
+		} else {
+			
+			var atIn = true;
+			
+		};
+		
+		//at2
+		var atThere2 = address.lastIndexOf("@");
+		
+		if (atThere2 === atThere) {
+			
+			var atCheck = true;
+			
+		} else {
+			
+			var atCheck = false;
+			
+		};
+		
+		//at verify
+		if (atIn && atCheck) {
+			
+			var atIncluded = true;
+			
+		} else {
+			
+			var atIncluded = false;
+			
+		};
+		
+		//period check
+		var perThere = address.indexOf(".");
+		
+		if (perThere === -1) {
+			
+			var perIncluded = false;
+			
+		} else {
+			
+			var perIncluded = true;
+			
+		};
+		
+		//final check	
+		if (atIncluded && perIncluded) {
+			
+			var confirm = true;
+			
+		} else {
+			
+			var confirm = false;
+			
+		};
+		
+		return confirm;
+		
+	};
 	
+	//callout
+	end = addConfirm(text);
+	
+	//result log	
+	if (end === true) {
+		
+		console.log("\"" + text + "\" is confirmed as a plausible email address.");
+		
+	} else {
+		
+		console.log("The entered address is invalid.  Check the address and try again.");
+	
+	};
+*/
+
+//problem 2
+
+//variable
+var list = "a,b,c,d";
+var divide1 = ",";
+var divide2 = "/";
+var listEnd = "";
+
+//function
+
+var splitList = function (listString) {
+	
+	var spread = listString.split(divide1);
+
+	for (var index in spread) {
+			
+			listEnd = listEnd + spread[index] + divide2;
+			
+		}
 	
 };
 
-addConfirm(text);
+//callout
 
-//problem 2
+splitList(list);
+
+//result
+
+console.log(listEnd);
