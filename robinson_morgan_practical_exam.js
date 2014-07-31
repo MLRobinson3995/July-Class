6b19dee826727502507a2d3aa88f743aa180c7bb
MLRobinson3995 <MLRobinson@fullsail.edu>
2014-07-31 17:55:01 -0400

    7/31/2014 SDI practical exam Problem 1 inital.

---
+++ robinson_morgan_practical_exam.js
@@ -0,0 +1,17 @@
+///  Morgan Robinson
+//  07/31/2014
+// Scalable Data Infrastructures 1407
+//  Practical Exam
+
+//Problem 1
+
+//variables
+
+var array1 = new Array(1, "Pickles", 3, "onions", 5, "10", 6, "SDI");
+
+//function
+
+//callout
+
+
+ console.log(array1);
